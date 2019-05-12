1e9e8734f453385c6e5fdb0e5fe1a2db7d44ffd6
Hua Huang <huangh223@gatech.edu>
2019-05-12 11:36:29 -0400

    Update test_ID_compress.c & H2P_partial_pivot_QR()

--- src/H2Pack_ID_compress.c
+++ src/H2Pack_ID_compress.c
@@ -33,79 +33,72 @@ void H2P_partial_pivot_QR(
 )
 {
     DTYPE *R = A->data;
-    const int nrow = A->nrow;
-    const int ncol = A->ncol;
-    const int ldR  = A->ld;
-    const int max_iter = MIN(nrow, ncol);
+    int nrow = A->nrow;
+    int ncol = A->ncol;
+    int ldR  = A->ld;
+    int max_iter = MIN(nrow, ncol);
     
     DTYPE *col_norm = (DTYPE*) H2P_malloc_aligned(sizeof(DTYPE) * ncol);
-    DTYPE *col_tmp  = (DTYPE*) H2P_malloc_aligned(sizeof(DTYPE) * nrow);
-    DTYPE *h_vec    = (DTYPE*) H2P_malloc_aligned(sizeof(DTYPE) * nrow);
     DTYPE *h_R_mv   = (DTYPE*) H2P_malloc_aligned(sizeof(DTYPE) * ncol);
-    assert(col_norm != NULL && col_tmp != NULL);
-    assert(h_vec    != NULL && h_R_mv  != NULL);
-    DTYPE max_col_norm = 0.0;
-    for (int icol = 0; icol < ncol; icol++)
+    assert(col_norm != NULL && h_R_mv != NULL);
+    DTYPE norm_p = 0.0;
+    int pivot = 0;
+    for (int j = 0; j < ncol; j++)
     {
-        p[icol] = icol;
-        col_norm[icol] = CBLAS_NRM2(nrow, R + icol * ldR, 1);
-        max_col_norm = MAX(col_norm[icol], max_col_norm);
+        p[j] = j;
+        col_norm[j] = CBLAS_NRM2(nrow, R + j * ldR, 1);
+        // 1. Find a column with largest 2-norm
+        if (col_norm[j] > norm_p)
+        {
+            norm_p = col_norm[j];
+            pivot = j;
+        }
     }
     
     int stop_rank = MIN(max_iter, tol_rank);
     DTYPE norm_eps = DSQRT((DTYPE) nrow) * 1e-15;
     DTYPE stop_norm = MAX(norm_eps, tol_norm);
-    if (rel_norm) stop_norm *= max_col_norm;
+    if (rel_norm) stop_norm *= norm_p;
     
     int rank = -1;
-    size_t col_msize = sizeof(DTYPE) * nrow;
     // Main iteration of Household QR
     for (int iter = 0; iter < max_iter; iter++)
-    {
-        // 1. Find a column with largest 2-norm
-        DTYPE norm_p = col_norm[iter];
-        int   pivot  = iter;
-        for (int j = iter + 1; j < ncol; j++)
-        {
-            if (col_norm[j] > norm_p)
-            {
-                norm_p = col_norm[j];
-                pivot  = j;
-            }
-        }
-        
-        // 2. Check the stop criteria
+    {   
+        // 1. Check the stop criteria
         if ((norm_p < stop_norm) || (iter >= stop_rank))
         {
             rank = iter;
             break;
         }
         
-        // 3. Swap the column
+        // 2. Swap the column
         if (iter != pivot)
         {
             int tmp_p = p[iter]; p[iter] = p[pivot]; p[pivot] = tmp_p;
             DTYPE tmp_norm  = col_norm[iter];
             col_norm[iter]  = col_norm[pivot];
             col_norm[pivot] = tmp_norm;
-            memcpy(col_tmp,         R + iter  * ldR, col_msize);
-            memcpy(R + iter  * ldR, R + pivot * ldR, col_msize);
-            memcpy(R + pivot * ldR, col_tmp,         col_msize);
+            DTYPE *R_iter  = R + iter  * ldR;
+            DTYPE *R_pivot = R + pivot * ldR;
+            for (int j = 0; j < nrow; j++)
+            {
+                DTYPE tmp_R = R_iter[j];
+                R_iter[j]   = R_pivot[j];
+                R_pivot[j]  = tmp_R;
+            }
         }
         
-        // 4. Householder orthogonalization
-        int h_len = nrow - iter;
-        memcpy(h_vec, R + iter * ldR + iter, sizeof(DTYPE) * h_len);
-        DTYPE sign = (h_vec[0] > 0.0) ? 1.0 : -1.0;
+        // 3. Householder orthogonalization
+        int h_len    = nrow - iter;
+        DTYPE *h_vec = R + iter * ldR + iter;
+        DTYPE sign   = (h_vec[0] > 0.0) ? 1.0 : -1.0;
         DTYPE h_norm = CBLAS_NRM2(h_len, h_vec, 1);
         h_vec[0] = h_vec[0] + sign * h_norm;
         DTYPE inv_h_norm = 1.0 / CBLAS_NRM2(h_len, h_vec, 1);
         CBLAS_SCAL(h_len, inv_h_norm, h_vec, 1);
         
-        // 5. Eliminate the iter-th sub-column and orthogonalize columns 
+        // 4. Eliminate the iter-th sub-column and orthogonalize columns 
         //    right to the iter-th column
-        R[iter * ldR + iter] = -sign * h_norm;
-        memset(R + iter * ldR + iter + 1, 0, sizeof(DTYPE) * (h_len - 1));
         DTYPE *R_block = R + (iter + 1) * ldR + iter;
         int R_block_nrow = h_len;
         int R_block_ncol = ncol - iter - 1;
@@ -117,9 +110,14 @@ void H2P_partial_pivot_QR(
             CblasColMajor, R_block_nrow, R_block_ncol, -2.0,
             h_vec, 1, h_R_mv, 1, R_block, ldR
         );
+        // We don't need h_vec anymore, can overwrite the iter-th column of R
+        R[iter * ldR + iter] = -sign * h_norm;
+        memset(R + iter * ldR + (iter + 1), 0, sizeof(DTYPE) * (h_len - 1));
         
-        // 6. Update 2-norm of columns right to the iter-th column
+        // 5. Update 2-norm of columns right to the iter-th column and find next pivot
         int h_len_m1 = h_len - 1;
+        pivot  = iter + 1;
+        norm_p = 0.0;
         for (int j = iter + 1; j < ncol; j++)
         {
             // Skip small columns
@@ -141,14 +139,18 @@ void H2P_partial_pivot_QR(
             } else {
                 col_norm[j] = DSQRT(tmp);
             }
+            
+            if (col_norm[j] > norm_p)
+            {
+                norm_p = col_norm[j];
+                pivot  = j;
+            }
         }
     }
     if (rank == -1) rank = max_iter;
     
     *r = rank;
-    H2P_free_aligned(h_vec);
     H2P_free_aligned(h_R_mv);
-    H2P_free_aligned(col_tmp);
     H2P_free_aligned(col_norm);
 }
 
--- tests/test_ID_compress.c
+++ tests/test_ID_compress.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <math.h>
 #include <mkl.h>
+#include <time.h>
 
 #include "H2Pack_utils.h"
 #include "H2Pack_dense_mat.h"
@@ -11,22 +12,45 @@
 int main()
 {
     int nrow, ncol;
+    printf("matrix size: ");
     scanf("%d %d", &nrow, &ncol);
-    H2P_dense_mat_t A, U;
+    H2P_dense_mat_t A, A0, U;
     H2P_dense_mat_init(&A, nrow, ncol);
+    H2P_dense_mat_init(&A0, nrow, ncol);
     
-    DTYPE delta_y = 1.0 / (DTYPE)(nrow - 1);
-    DTYPE delta_x = 1.0 / (DTYPE)(ncol - 1);
+    DTYPE A0_fnorm = 0.0;
+    srand(time(NULL));
+    DTYPE *x1 = (DTYPE*) malloc(sizeof(DTYPE) * nrow);
+    DTYPE *y1 = (DTYPE*) malloc(sizeof(DTYPE) * nrow);
+    DTYPE *x2 = (DTYPE*) malloc(sizeof(DTYPE) * ncol);
+    DTYPE *y2 = (DTYPE*) malloc(sizeof(DTYPE) * ncol);
+    for (int i = 0; i < nrow; i++) 
+    {
+        x1[i] = (DTYPE) rand() / (DTYPE) RAND_MAX;
+        y1[i] = (DTYPE) rand() / (DTYPE) RAND_MAX;
+    }
+    for (int i = 0; i < ncol; i++) 
+    {
+        x2[i] = (DTYPE) rand() / (DTYPE) RAND_MAX + 1.0;
+        y2[i] = (DTYPE) rand() / (DTYPE) RAND_MAX + 1.0;
+    }
     for (int irow = 0; irow < nrow; irow++)
     {
-        DTYPE *A_irow = A->data + irow * ncol;
-        DTYPE y_irow = delta_y * irow;
+        DTYPE *A_irow  = A->data + irow * ncol;
+        DTYPE *A0_irow = A0->data + irow * ncol;
         for (int icol = 0; icol < ncol; icol++)
         {
-            DTYPE x_icol = 0.5 + delta_x * icol;
-            A_irow[icol] = 1.0 / (1.0 + x_icol - y_irow);
+            DTYPE dx = x1[irow] - x2[icol];
+            DTYPE dy = y1[irow] - y2[icol];
+            DTYPE d  = sqrt(dx * dx + dy * dy);
+            A_irow[icol]  = 1.0 / d;
+            A0_irow[icol] = A_irow[icol];
+            A0_fnorm += A_irow[icol] * A_irow[icol];
         }
     }
+    A0_fnorm = sqrt(A0_fnorm);
+    mkl_set_num_threads(1);
+    
     FILE *ouf = fopen("A.csv", "w");
     for (int irow = 0; irow < nrow; irow++)
     {
@@ -43,19 +67,25 @@ int main()
 
     int *J = (int*) malloc(sizeof(int) * nrow);
     DTYPE tol_norm;
-    printf("tol_rank: ");
+    printf("norm_rel_tol: ");
     scanf("%lf", &tol_norm);
-    //int nrank;
-    //printf("rank:");
-    //scanf("%d", &nrank);
-    mkl_set_num_threads(1);
-    double st = H2P_get_wtime_sec();
-    H2P_ID_compress(A, QR_REL_NRM, &tol_norm, &U, J);
-    double et = H2P_get_wtime_sec();
-    printf("U rank = %d, used time = %.4lf (s)\n", U->ncol, et - st);
+    H2P_ID_compress(A, QR_REL_NRM, &tol_norm, &U, J);  // Warm up
+    double ut = 0.0;
+    for (int i = 0; i < 10; i++)
+    {
+        memcpy(A->data, A0->data, sizeof(DTYPE) * nrow * ncol);
+        A->nrow = nrow;
+        A->ncol = ncol;
+        A->ld = ncol;
+        double st = H2P_get_wtime_sec();
+        H2P_ID_compress(A, QR_REL_NRM, &tol_norm, &U, J);
+        double et = H2P_get_wtime_sec();
+        ut += et - st;
+    }
+    printf("U rank = %d, average used time = %.8lf (s)\n", U->ncol, ut / 10.0);
     fflush(stdout);
+    
     ouf = fopen("U.csv", "w");
-    printf("U: \n");
     for (int irow = 0; irow < U->nrow; irow++)
     {
         DTYPE *U_irow = U->data + irow * U->ncol;
@@ -69,12 +99,30 @@ int main()
     }
     fclose(ouf);
     
-    printf("J: ");
-    for (int icol = 0; icol < U->ncol; icol++) printf("%d ", J[icol]);
-    printf("\n");
+    //printf("A skeleton rows: ");
+    //for (int i = 0; i < U->ncol; i++) printf("%d ", J[i]);
+    //printf("\n");
+    
+    DTYPE *AJ = (DTYPE*) malloc(sizeof(DTYPE) * ncol * U->ncol);
+    for (int i = 0; i < U->ncol; i++)
+        memcpy(AJ + i * ncol, A0->data + J[i] * ncol, sizeof(DTYPE) * ncol);
+    cblas_dgemm(
+        CblasRowMajor, CblasNoTrans, CblasNoTrans, nrow, ncol, U->ncol,
+        1.0, U->data, U->ncol, AJ, ncol, -1.0, A0->data, A0->ncol
+    );
+    DTYPE res_fnorm = 0.0;
+    for (int i = 0; i < nrow * ncol; i++) 
+        res_fnorm += A0->data[i] * A0->data[i];
+    res_fnorm = sqrt(res_fnorm);
+    printf("||A - A_{H2}||_fro / ||A||_fro = %e\n", res_fnorm / A0_fnorm);
     
     free(J);
+    free(x1);
+    free(y1);
+    free(x2);
+    free(y2);
     H2P_dense_mat_destroy(U);
     H2P_dense_mat_destroy(A);
+    H2P_dense_mat_destroy(A0);
     return 0;
 }
